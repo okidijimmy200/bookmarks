86039c854f85fe0e9a4e3785ebb9ba100917cfd1
okidijimmy200 <okidijimmie@gmail.com>
2020-09-30 15:21:40 +0300

    Adding generic relations to your models

--- actions/models.py
+++ actions/models.py
@@ -8,17 +8,21 @@ class Action(models.Model):
 # user: The user who performed the action; this is a ForeignKey to the Django User model.
     user = models.ForeignKey('auth.User',
                             related_name='actions',
-                            db_index=models.CASCADE)
+                            db_index=True,
+                            on_delete=models.CASCADE)
 # verb: The verb describing the action that the user has performed.
     verb = models.CharField(max_length=255)
+# This will tell you the model for the relationship and points to the ContentType model
     target_ct = models.ForeignKey(ContentType,
                                     blank=True,
                                     null=True,
                                     related_name='target_obj',
                                     on_delete=models.CASCADE)
+#PositiveIntegerField to match Django's automatic primary key fields ie stroing the primary key
     target_id = models.PositiveIntegerField(null=True,
                                             blank=True,
                                            db_index=True)
+# A field to define and manage the generic relation using the two previous fields
     target = GenericForeignKey('target_ct', 'target_id')
 # created: The date and time when this action was created. You use auto_ now_add=True to automatically set this to the current datetime when the
 # object is saved for the first time in the database.
---
+++ actions/utils.py
@@ -0,0 +1,42 @@
+import datetime
+from django.utils import timezone
+from django.contrib.contenttypes.models import ContentType
+from .models import Action
+
+# The create_action() function allows you to create actions that optionally include
+# a target object. this can be used in your code as a shortcut to
+# add new actions to the activity stream.
+def create_action(user, verb, target=None):
+     # check for any similar action made in the last minute
+# Sometimes, your users might click several times on the LIKE or UNLIKE button or perform the same action multiple times in a short period of time. This will easily
+# lead to storing and displaying duplicate actions
+     now = timezone.now()
+     last_minute = now - datetime.timedelta(seconds=60)
+     similar_actions = Action.objects.filter(user_id=user.id,
+                                            verb=verb,
+                                            created_gte=last_minute)
+
+    if target:
+        target_ct = ContentType.objects.get_for_model(target)
+        similar_actions = similar_actions.filter(
+                                            target_ct=target_ct,
+                                            target_id=target.id
+        )
+
+    if not similar_actions:
+    # no existing actions found
+        action = Action(user=user, verb=verb, target=target)
+        action.save()
+        return save()
+        return True
+    return False
+
+'''
+to avoid duplicate actions
+1)First, you get the current time using the timezone.now() method provided
+by Django.
+2)You use the last_minute variable to store the datetime from one minute
+ago and retrieve any identical actions performed by the user since then.
+3)You create an Action object if no identical action already exists in the last
+minute. You return True if an Action object was created, or False otherwise.
+'''
